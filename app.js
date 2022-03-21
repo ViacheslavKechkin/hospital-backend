4e1589480a7ba08df9a48383f8762e6aa29869a2
vykechkin <vykechkin.exceedteam@gmail.com>
2022-03-21 15:23:27 +0300

    fix comments

--- app.js
+++ app.js
@@ -4,11 +4,8 @@ const mongoose = require("mongoose");
 const app = express();
 const PORT = 9000;
 
-// const apiRoutes = require("./src/modules/routes/routes");
-
 app.use(cors());
 app.use(express.json());
-// app.use("/", apiRoutes);
 
 const uri = "mongodb+srv://vykechkin:kechkin123@cluster0.w2ver.mongodb.net/Hospital?retryWrites=true&w=majority";
 mongoose.connect(uri);
