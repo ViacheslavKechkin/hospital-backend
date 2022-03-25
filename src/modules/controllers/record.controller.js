c4dcc8f1adb7c94b0f67e319476bf1bc102477e4
vykechkin <vykechkin.exceedteam@gmail.com>
2022-03-25 15:03:03 +0300

    add-route-for-new-record

--- src/modules/controllers/record.controller.js
+++ src/modules/controllers/record.controller.js
@@ -5,3 +5,17 @@ module.exports.getAllRecords = (req, res) => {
     res.send({ data: result });
   });
 };
+
+module.exports.createRecord = (req, res) => {
+  const record = new Record(req.body);
+  const { body } = req;
+  if (body) {
+    record.save().then(() => {
+      Record.find().then(result => {
+        res.send({ data: result });
+      });
+    });
+  } else {
+    res.status(422).send('Error! Params not correct');
+  };
+};
