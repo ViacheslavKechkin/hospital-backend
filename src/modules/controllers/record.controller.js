7a93dd13ed99320e359f37f1d4f1c4e797d3fd02
vykechkin <vykechkin.exceedteam@gmail.com>
2022-03-28 10:20:30 +0300

    add .catch

--- src/modules/controllers/record.controller.js
+++ src/modules/controllers/record.controller.js
@@ -8,21 +8,17 @@ module.exports.getAllRecords = (req, res) => {
 
 module.exports.createRecord = (req, res) => {
 
-  try {
-    const record = new Record(req.body);
-    const { body } = req;
-    if (body) {
-      record.save().then(() => {
-        Record.find().then(result => {
-          res.send({ data: result });
-        });
+  const record = new Record(req.body);
+  const { body } = req;
+  if (body) {
+    record.save().then(() => {
+      Record.find().then(result => {
+        res.send({ data: result });
       });
-    } else {
-      res.status(422).send('Error! Params not correct');
-    };
-  }
-  catch (e) {
-    res.status(400).send('Error adding new entry!');
-  }
-
+    }).catch((res) => {
+      res.status(400).send('Error adding new entry!');
+    });
+  } else {
+    res.status(422).send('Error! Params not correct');
+  };
 };
